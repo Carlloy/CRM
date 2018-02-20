b19e48c3d3b646c89797c30bc6a1f5154cb9c8ed
Konrad Chojnecki <konrad.chojnecki@best.wroclaw.pl>
2018-02-20 21:39:58 +0100

    change test

--- src/test/java/crm/app/data/dao/AppUserDAOTest.java
+++ src/test/java/crm/app/data/dao/AppUserDAOTest.java
@@ -2,12 +2,19 @@ package crm.app.data.dao;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppUserDAO.class})
 public class AppUserDAOTest {
+    @Autowired
+    AppUserDAO appUserDAO;
 
     @Test
     public void isEmailExists() {
-        AppUserDAO appUserDAO = new AppUserDAO();
         Boolean result = appUserDAO.isEmailExists("admin@gmail.com");
         Assert.assertTrue(result);
     }
