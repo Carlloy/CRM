7c5765f3f06af6c8fe28c83112def57233d7bd29
Konrad Chojnecki <konrad.chojnecki@best.wroclaw.pl>
2018-02-20 18:45:45 +0100

    implement passwordEncoder with Bcrypt

--- src/main/java/crm/app/services/registration/PasswordEncoder.java
+++ src/main/java/crm/app/services/registration/PasswordEncoder.java
@@ -1,4 +1,12 @@
 package crm.app.services.registration;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PasswordEncoder {
+
+    public String encode(String password) {
+        return new BCryptPasswordEncoder().encode(password);
+    }
 }
